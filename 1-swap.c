8bf8aca3b43ad7f1aaa06333e3dc29bf3b97fb06
George20222 <aliinalegwu@gmail.com>
2022-10-25 13:37:27 -0700

    task c1

---
+++ 1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of 2 integers
+ * @a: int a
+ * @b: int b
+ */
+void swap_int(int *a, int *b);
+{
+	int q;
+
+
+	q = *a;
+	*a = *b;
+	*b = q;
+}
