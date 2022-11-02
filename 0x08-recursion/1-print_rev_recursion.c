1f6b9b847253611c11356b4017ef6e1753c4c872
George20222 <aliinalegwu@gmail.com>
2022-11-02 12:09:01 -0700

    first rcurse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,12 +5,11 @@
  * @s: string
  * Return: no return.
  */
-void _puts_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-	
 }
